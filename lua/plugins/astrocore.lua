82f9fcfad991f2f569c1cc008cf3f75d8835642b
zwphyro <tofumaks@gmail.com>
2024-09-02 02:14:16 +0300

    base config

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroCore provides a central place to modify mappings, vim options, autocommands, and more!
 -- Configuration documentation can be found with `:h astrocore`
@@ -32,6 +32,11 @@ return {
         spell = false, -- sets vim.opt.spell
         signcolumn = "yes", -- sets vim.opt.signcolumn to yes
         wrap = false, -- sets vim.opt.wrap
+
+        tabstop = 4,
+        softtabstop = 4,
+        shiftwidth = 4,
+        scrolloff = 20,
       },
       g = { -- vim.g.<key>
         -- configure global vim variables (vim.g)
